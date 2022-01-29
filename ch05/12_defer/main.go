186772fc2f7b2c3ce8397ee23763c5a0e5ad46a5
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-01-29 11:13:45 +0330

    Defer

---
+++ ch05/12_defer/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("no file specified")
+	}
+
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close() // delayed invocation until the surrounding function exits
+	// NOTE: defers run in Lat In First Out order (LIFO or stack)
+
+	data := make([]byte, 2048)
+	for {
+		count, err := f.Read(data)
+		os.Stdout.Write(data[:count])
+		if err != nil {
+			if err != io.EOF {
+				log.Fatal(err)
+			}
+			// We're at the end of the file
+			break
+		}
+	}
+}
