b626d9ea1b4e5aa28e804764a3f76c6b41e7c358
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-12-04 02:40:53 +0330

    Use table tests pattern to make tests less repetetive.

---
+++ ch13/05_table_tests/table.go
@@ -0,0 +1,24 @@
+package table
+
+import (
+	"errors"
+	"fmt"
+)
+
+func DoMath(a, b int, op string) (int, error) {
+	switch op {
+	case "+":
+		return a + b, nil
+	case "-":
+		return a - b, nil
+	case "*":
+		return a * b, nil
+	case "/":
+		if b == 0 {
+			return 0, errors.New("division by zero")
+		}
+		return a / b, nil
+	default:
+		return 0, fmt.Errorf("unknown operator %s", op)
+	}
+}
---
+++ ch13/05_table_tests/table_test.go
@@ -0,0 +1,43 @@
+package table
+
+import (
+	"testing"
+)
+
+func TestDoMath(t *testing.T) {
+	data := []struct {
+		name       string
+		num1, num2 int
+		op         string
+		expected   int
+		errMsg     string
+	}{
+		{"addition", 4, 9, "+", 13, ""},
+		{"subtraction", 9, 4, "-", 5, ""},
+		{"multiplication", 4, 9, "*", 36, ""},
+		{"division", 26, 2, "/", 13, ""},
+		{"bad_division", 4, 0, "/", 0, "division by zero"},
+	}
+
+	for _, d := range data {
+		t.Run(d.name, func(t *testing.T) {
+			result, err := DoMath(d.num1, d.num2, d.op)
+			if result != d.expected {
+				t.Errorf("Expected %d, got %d", d.expected, result)
+			}
+			// NOTE: Comparing error messages can be fragile, because there may not
+			// be any compatibility guarantees on the message text. The function that
+			// we are testing uses errors.New and fmt.Errorf to make errors, so the
+			// only option is to compare the messages. If an error has a custom type,
+			// use errors.Is or errors.As to check that the correct error is
+			// returned.
+			var errMsg string
+			if err != nil {
+				errMsg = err.Error()
+			}
+			if errMsg != d.errMsg {
+				t.Errorf("Expected error message: `%s`, got `%s`", d.errMsg, errMsg)
+			}
+		})
+	}
+}
