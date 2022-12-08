e99e2ca98636ca50da201372f73a9ca53da6fc72
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-12-08 18:46:09 +0330

    Update the code to make the generic type comparable.

---
+++ ch15/01_introducing_generics/go.mod
@@ -0,0 +1,3 @@
+module github.com/exvimmer/introducing_generics
+
+go 1.19
--- ch15/01_introducing_generics/main.go
+++ ch15/01_introducing_generics/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-type Stack[T any] struct {
+type Stack[T comparable] struct {
 	vals []T
 }
 
@@ -20,6 +20,15 @@ func (s *Stack[T]) Pop() (T, bool) {
 	return back, true
 }
 
+func (s Stack[T]) Contains(val T) bool {
+	for _, v := range s.vals {
+		if v == val {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
 	var intStack Stack[int]
 	intStack.Push(10)
@@ -27,4 +36,7 @@ func main() {
 	intStack.Push(30)
 	v, ok := intStack.Pop()
 	fmt.Println(v, ok) // 30, true
+
+	fmt.Printf("contains 20: %t\n", intStack.Contains(20))
+	fmt.Printf("contains 30: %t\n", intStack.Contains(30))
 }
