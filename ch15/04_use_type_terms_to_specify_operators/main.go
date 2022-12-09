81fd6b3b1b7652b693a454917774dd55f6a40b4f
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-12-09 13:53:02 +0330

    Make type term valid for any type that has the type term as its underlying type, by using a tilde (~) before the type term.

--- ch15/04_use_type_terms_to_specify_operators/main.go
+++ ch15/04_use_type_terms_to_specify_operators/main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 type BuiltInOrdered interface {
-	int | int8 | int16 | int32 | int64 | string | float64 | float32 | uint |
-		uint8 | uint16 | uint32 | uint64 | uintptr
+	~int | ~int8 | ~int16 | ~int32 | ~int64 | ~string | ~float64 | ~float32 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
 }
 
 func Min[T BuiltInOrdered](a, b T) T {
@@ -20,9 +20,9 @@ func main() {
 	a, b := 10, 20
 	fmt.Println(Min(a, b))
 
-	// var (
-	// 	MyA MyInt = 13
-	// 	MyB MyInt = 99
-	// )
-	// fmt.Println(Min(MyA, MyB)) // ERROR
+	var (
+		MyA MyInt = 13
+		MyB MyInt = 99
+	)
+	fmt.Println(Min(MyA, MyB))
 }
